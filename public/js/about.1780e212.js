1bb611e915afe0f57e8142a29343f7ec890bb131
Alexis Riot <riotalexis35@gmail.com>
2020-08-13 19:36:39 +0200

    Initializing application with Laravel, VueJS (vue-cli, vuetify, vuex, vuerouter)

---
+++ public/js/about.1780e212.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var c=e("5c40"),s={class:"about"},a=Object(c["j"])("h1",null,"This is an about page",-1);function o(n,t){return Object(c["s"])(),Object(c["e"])("div",s,[a])}const u={};u.render=o;t["default"]=u}}]);
+//# sourceMappingURL=about.1780e212.js.map
